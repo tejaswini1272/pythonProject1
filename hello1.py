06673cde0466798bf9c6eb8ffc0cef1901050b0c
prave <tejaswini536.tgmail.com>
2022-04-14 14:28:51 +0530

    Comment1

--- hello1.py
+++ hello1.py
@@ -4,4 +4,3 @@ from selenium import webdriver
 driver=webdriver.Chrome(executable_path="C:\\User\\prave\\chromedriver_win32\\chromedriver.exe")
 
 driver.get("https//www.google.com")
-print("Tejaswini123")
