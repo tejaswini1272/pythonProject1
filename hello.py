9b1b1a7c7e08d9e8b13952da4528b7234519f09c
prave <tejaswini536.tgmail.com>
2022-04-14 14:18:23 +0530

    heel

---
+++ hello.py
@@ -0,0 +1,6 @@
+print("hello world")
+from selenium import webdriver
+
+driver=webdriver.Chrome(executable_path="C:\\User\\prave\\chromedriver_win32\\chromedriver.exe")
+
+driver.get("https//www.google.com")
